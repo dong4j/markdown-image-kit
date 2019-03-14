fe0145abf442ef9e713049033e3feb45be8530e6
dong4j <dong4j@gmail.com>
2019-03-14 16:51:25 +0800

    :wrench: 修改配置文件

---
+++ src/main/java/info/dong4j/idea/plugin/action/WeiboObjectStorageServiceAction.java
@@ -0,0 +1,21 @@
+package info.dong4j.idea.plugin.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * <p>Company: 科大讯飞股份有限公司-四川分公司</p>
+ * <p>Description: </p>
+ *
+ * @author dong4j
+ * @email sjdong3@iflytek.com
+ * @since 2019-03-14 16:39
+ */
+public final class WeiboObjectStorageServiceAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
