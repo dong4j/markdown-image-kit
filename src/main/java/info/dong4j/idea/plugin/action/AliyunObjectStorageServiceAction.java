e95c85f87e89d7218a6a296a001f29ec27e12525
dong4j <dong4j@gmail.com>
2019-03-18 00:42:27 +0800

    :mute: 删除 AliyunObjectStorageServiceAction.actionPerformed

--- src/main/java/info/dong4j/idea/plugin/action/AliyunObjectStorageServiceAction.java
+++ src/main/java/info/dong4j/idea/plugin/action/AliyunObjectStorageServiceAction.java
@@ -45,52 +45,6 @@ public final class AliyunObjectStorageServiceAction extends AbstractObjectStorag
         return OssPersistenConfig.getInstance().getState().getAliyunOssState().isPassedTest();
     }
 
-    /**
-     * 处理点击 "upload to Aliyun OSS" 按钮的逻辑
-     *
-     * @param anActionEvent the an action event
-     */
-    // @Override
-    // public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-    //     Project project = anActionEvent.getProject();
-    //     if (project != null) {
-    //         log.trace("project's base path = {}", project.getBasePath());
-    //         // 获取当前操作的文件
-    //         PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
-    //         if (psiFile != null && isMardownFile(psiFile.getOriginalFile().getName())) {
-    //             // 解析 markdown 中的图片标签
-    //             Document document = psiFile.getViewProvider().getDocument();
-    //             String text = Objects.requireNonNull(document).getText();
-    //
-    //             // 用回车键来分隔几个元素
-    //             String[] textArray = text.split("\n");
-    //             String url = "";
-    //             for (int i = 0; i < textArray.length; i++) {
-    //                 // todo-dong4j : (2019年03月12日 19:02) [要求只是是图片表示, 则肯定是以 ![]() 的形式出现]
-    //                 if (StringUtils.isNotBlank(textArray[i]) && textArray[i].trim().startsWith("![") && textArray[i].trim().endsWith(")")) {
-    //                     log.trace(textArray[i]);
-    //                     // 替换字符串
-    //                     Map<String, String> map = ParserUtils.parseImageTag(textArray[i]);
-    //                     for (Map.Entry<String, String> result : map.entrySet()) {
-    //                         log.trace("key = {}, value = {}", result.getKey(), result.getValue());
-    //                         // 上传到 OSS
-    //                         url = upload(anActionEvent, result.getValue());
-    //                         textArray[i] = ParserUtils.parse0(HTML_TAG_EXTEND, result.getKey(), url, result.getKey(), url);
-    //                     }
-    //                 }
-    //             }
-    //
-    //             // 替换全部字符串
-    //             StringBuilder stringBuilder = new StringBuilder();
-    //             for (String string : textArray) {
-    //                 stringBuilder.append(string).append("\n");
-    //             }
-    //             PsiDocumentUtils.commitAndSaveDocument(project, document, stringBuilder.toString());
-    //             // notifucation(url);
-    //         }
-    //     }
-    // }
-
     private String upload(AnActionEvent anActionEvent, String filePath) {
         final Project project = anActionEvent.getProject();
         PsiFile psiFile = findImageResource(project, filePath);
