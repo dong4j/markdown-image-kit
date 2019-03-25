678b1caa0a1afac416e506b159c76180094f9c98
dong4j <dong4j@gmail.com>
2019-03-25 16:16:32 +0800

    设置图片默认保存路径

--- src/main/java/info/dong4j/idea/plugin/settings/ProjectSettingsPage.java
+++ src/main/java/info/dong4j/idea/plugin/settings/ProjectSettingsPage.java
@@ -142,7 +142,6 @@ public class ProjectSettingsPage implements SearchableConfigurable, Configurable
     /** clipboard group */
     private JCheckBox clipboardControlCheckBox;
     private JCheckBox copyToDirCheckBox;
-    // todo-dong4j : (2019年03月22日 21:45) [设置默认路径 ./imgs]
     private JTextField whereToCopyTextField;
     private JCheckBox uploadAndReplaceCheckBox;
     private JComboBox defaultCloudComboBox;
---
+++ src/test/java/info/dong4j/idea/plugin/client/HelpTest.java
@@ -0,0 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 dong4j <dong4j@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package info.dong4j.idea.plugin.client;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.junit.Test;
+
+/**
+ * <p>Company: 科大讯飞股份有限公司-四川分公司</p>
+ * <p>Description: </p>
+ *
+ * @author dong4j
+ * @email sjdong3@iflytek.com
+ * @since 2019-03-25 13:20
+ */
+public class HelpTest {
+
+    private HttpClient help(String where) throws Exception {
+        HttpClient client = new HttpClient();
+        PostMethod method = new PostMethod("http://127.0.0.1:8080/rest/help/" + where);
+        client.getParams().setContentCharset("UTF-8");
+        client.executeMethod(method);
+        String response;
+        try {
+            response = method.getResponseBodyAsString(1000);
+        } finally {
+            method.releaseConnection();
+        }
+        if (response == null) {
+            throw new NullPointerException();
+        }
+        return client;
+    }
+
+    @Test
+    public void test() {
+        try {
+            help("setting");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
