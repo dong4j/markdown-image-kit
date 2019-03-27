9de7b7de44b1773de6ef9c6d7c61e1ac5e2ff9f3
dong4j <dong4j@gmail.com>
2019-03-27 21:25:58 +0800

    :sparkles: 添加文件重命名处理链

---
+++ src/main/java/info/dong4j/idea/plugin/chain/ImageRenameHandler.java
@@ -0,0 +1,37 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 dong4j <dong4j@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package info.dong4j.idea.plugin.chain;
+
+/**
+ * <p>Company: 科大讯飞股份有限公司-四川分公司</p>
+ * <p>Description: 图片文件重命名</p>
+ *
+ * @author dong4j
+ * @email sjdong3@iflytek.com
+ * @since 2019-03-27 21:24
+ */
+public class ImageRenameHandler {
+}
